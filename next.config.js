25bd42bac121cf59a20fe32851b780d71f791c5f
Alan Pledger <alan@makeswift.com>
2023-05-10 12:08:53 -0400

    add headers test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "x-hello",
+            value: "world",
+          },
+        ],
+      },
+    ]
+  },
+  trailingSlash: true,
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react"
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  useEffect(() => {
+    fetch("/api/hello")
+  }, [])
+
+  return (
+    <p>
+      Look in the network tab for the <code>x-hello</code> response header for
+      the request to <code>/api/hello</code>. It is present locally, but not
+      when deployed to Vercel.
+    </p>
+  )
 }
